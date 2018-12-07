9daa275339b2c20d9a4c24d05626f05f9c3fecc1
Karan Singhal <karan1149@gmail.com>
2018-12-06 19:17:06 -0800

    Working classifier

--- classify.py
+++ classify.py
@@ -1,39 +1,50 @@
 import argparse
 import pickle
 import sklearn
-from sklearn.neural_network import MLPClassifer
+from sklearn.neural_network import MLPClassifier
 from collections import Counter
 
 if __name__=='__main__':
 
 	parser = argparse.ArgumentParser(description='Run classification on a dataset.')
-	parser.add_argument('train_path',
+	parser.add_argument('--data_path', help="Path to full dataset. If provided, 90 percent used for train and 10 percent used for test. Other flags must be empty.")
+	parser.add_argument('--train_path',
 	                    help='Path to train pkl file. Should be an array of pairs to train on.')
-	parser.add_argument('test_path',
+	parser.add_argument('--test_path',
 	                    help='Path to test pkl file. Should be an array of pairs.')
 
 	args = parser.parse_args()
 
-	with open(args.train_path, 'rb') as f:
-		train_data = pickle.load(f)
+	if args.data_path:
+		with open(args.data_path, 'rb') as f:
+			data = pickle.load(f)
+		train_data = data[:int(len(data) * .9)]
+		test_data = data[int(len(data) * .9):]
+		assert(not args.train_path and not args.test_path)
+	else:
+		with open(args.train_path, 'rb') as f:
+			train_data = pickle.load(f)
 
-	with open(args.test_path, 'rb') as f:
-		test_data = pickle.load(f)
+		with open(args.test_path, 'rb') as f:
+			test_data = pickle.load(f)
+
+	print("Loaded data...")
+	print("Training examples: %d, test examples: %d" % (len(train_data), len(test_data)))
 
 	train_data_x, train_data_y = zip(*train_data)
 	test_data_x, test_data_y = zip(*test_data)
 
-	assert(len(train_data_x[0]) == len(train_data_y[0]))
+	assert(len(train_data_x[0]) == len(test_data_x[0]))
 
-	print("Dimensionality of train %d" % len(train_data_x[0]))
+	print("Dimensionality of embeddings %d" % len(train_data_x[0]))
 
-	clf = MLPClassifer(hidden_layer_size=(100,), verbose=True)
+	clf = MLPClassifier(hidden_layer_sizes=(), verbose=True)
 
 	print("Classes found in training data:")
-	print(Counter(train_data_y, most_common=True))
+	print(Counter(train_data_y).most_common())
 
 	print("Classes found in test data:")
-	print(Counter(test_data_y, most_common=True))
+	print(Counter(test_data_y).most_common())
 
 	print("Fitting on training data...")
 	clf.fit(train_data_x, train_data_y)
